c0cd112cf66b9f5157b69f3619921fbbcbe826f7
Ankitsharma991 <sharmaa7861@gmail.com>
2024-01-13 15:46:43 +0530

    intial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import item1 from "./assets/images/item1.png";
+import item2 from "./assets/images/item2.png";
+import item3 from "./assets/images/item3.png";
+import logo from "./assets/images/foodie_logo.png";
+function App() {
+  return (
+    <div>
+      <h1 className="text-red-400">Foodie Home</h1>
+    </div>
+  );
+}
+
+export default App;
