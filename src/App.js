a73e03b455ab0153b9819f3e783b52d381aa82e5
Ankitsharma991 <sharmaa7861@gmail.com>
2024-01-14 17:22:16 +0530

    updated hompage structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
 import Navbar from "./component/Navbar/Navbar";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./component/Homepage/Homepage";
 import Footer from "./component/Footer/Footer.js";
 
@@ -15,11 +11,13 @@ function App() {
         <div className="mb-10">
           <Navbar />
         </div>
-        <Routes>
-          <Route path="/" Component={Homepage} />
-        </Routes>
-        <Footer />
-      </div>
+        {/* <div className="mt-10"> */}
+          <Routes>
+            <Route path="/" Component={Homepage} />
+          </Routes>
+          <Footer />
+        </div>
+      {/* </div> */}
     </Router>
   );
 }
